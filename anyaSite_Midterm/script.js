2b4f4932a50f04d15766312eb88570a98f5faa75
anya <aa10738@nyu.edu>
2025-04-16 11:28:42 -0400

    forgot to commit to repo until now ahhhh

--- anyaSite_Midterm/script.js
+++ anyaSite_Midterm/script.js
@@ -1,3 +1,17 @@
 document.getElementById("hamburger").addEventListener("click", function() {
     document.getElementById("menu").classList.toggle("show");
 });
+
+const onion = document.getElementById("clickOnion");
+const counter = document.getElementById("clickCounter");
+let onionClicks = 0;
+
+if (onion) {
+    onion.addEventListener("click", () => {
+        onionClicks++;
+        counter.textContent = `onion clicks: ${onionClicks}`;
+        if (onionClicks === 10) {
+            alert("you've unleashed the onion's power.");
+        }
+    });
+}
